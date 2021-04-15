cccbaff2d0e1989f59591dac1432827a8af4f085
Shane Howearth <Shane.Howearth@shanehowearth.com>
2021-04-16 09:13:29 +1000

    Or done channel example

---
+++ ordonechannel/ordonechannel.go
@@ -0,0 +1,25 @@
+// Package ordone -
+package ordone
+
+// OrDone -
+func OrDone(done, c <-chan interface{}) <-chan interface{} {
+	valStream := make(chan interface{})
+	go func() {
+		defer close(valStream)
+		for {
+			select {
+			case <-done:
+				return
+			case v, ok := <-c:
+				if ok == false {
+					return
+				}
+				select {
+				case valStream <- v:
+				case <-done:
+				}
+			}
+		}
+	}()
+	return valStream
+}
